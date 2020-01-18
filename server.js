8e5657f27d50234e51899ed30fc6e4429991bf80
Wei Lu <wadelu0714@gmail.com>
2020-01-18 19:32:36 +1300

    test commit

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const app = express()
 const expressLayouts = require('express-ejs-layouts')
 
 const indexRouter = require('./routes/index')
+const authorRouter = require('./routes/authors')
 
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
@@ -17,7 +18,8 @@ app.use(express.static('public'))
 
 const mongoose = require('mongoose')
 mongoose.connect(process.env.DATABASE_URL, {
-  useNewUrlParser: true
+  useNewUrlParser: true,
+  useUnifiedTopology: true
 })
 
 const db = mongoose.connection
@@ -26,5 +28,6 @@ db.on('error', error => console.error(error))
 db.once('open', () => console.log('Connected to mongoose'))
 
 app.use('/', indexRouter)
+app.use('/authors', authorRouter)
 
 app.listen(process.env.PORT || 3000)
